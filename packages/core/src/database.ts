fcdc99f0b583f57ddf3b554c706e1aba6a54368a
David Taing <dave@teeang.net>
2024-05-19 13:59:22 +1000

    perf: specify 1 second timeout for DynamoDB client
    see:
    - How to set timeout on AWS SDK v3 https://stackoverflow.com/a/69081346
    - 7 Common DynamoDB Patterns for Modeling and Building an App with Alex
      De Brie @ getClient function to improve the performance
      https://youtu.be/Q6-qWdsa8a4?si=WuSLyfR5rmeoF-vr&t=2762

--- packages/core/src/database.ts
+++ packages/core/src/database.ts
@@ -1,5 +1,19 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb'
+import { NodeHttpHandler } from '@smithy/node-http-handler'
 
-const dynamoClient = new DynamoDBClient()
+/**
+ * Dynamo has a default timeout of 30 seconds.
+ * Overriding timeouts to 1 second to improve perf.
+ *
+ * See: 7 Common DynamoDB Patterns for Modeling and Building an App with Alex
+ * De Brie @ getClient function to improve the performance
+ * https://youtu.be/Q6-qWdsa8a4?si=WuSLyfR5rmeoF-vr&t=2762
+ */
+const requestHandler = new NodeHttpHandler({
+  connectionTimeout: 1000,
+  requestTimeout: 1000,
+})
+
+const dynamoClient = new DynamoDBClient({ requestHandler })
 export const docClient = DynamoDBDocumentClient.from(dynamoClient)
